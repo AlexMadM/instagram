98deeaf0ec6b5397a1d6a1a63be476ccf4465dcb
AlexMadM <alex.alexandrov.1988@gmail.com>
2024-08-11 21:29:41 +0300

    UI button

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import React, {useState} from 'react';
 import {Modal} from "@/components/ui/modal";
+import {Button} from "@/components/ui/button/button";
 
 const Messages = () => {
     const [open, setOpen] =useState(false);
     return (
         <div>
-<button onClick={()=>setOpen(!open)}>dadsfa</button>
+<Button onClick={()=>setOpen(!open)}>dadsfa</Button>
             <Modal open={open} onOpenChange={setOpen} title="Modal">
                 <h1>Messagess</h1>
                 <p>ТЕКСТ</p>
