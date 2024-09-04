f40a694be9e86078a44058334533ec0c15117afa
AlexMadM <alex.alexandrov.1988@gmail.com>
2024-09-04 22:04:32 +0300

    UI

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+'use client';
 
-import Posts from "@/components/posts";
-import {Card} from "@/components/ui/card";
+import React from 'react';
+import car1 from '../../../../src/assets/imgs/car-1.jpg'
+import car2 from '../../../../src/assets/imgs/car-2.jpg'
+import car3 from '../../../../src/assets/imgs/car-3.jpg'
+import car4 from '../../../../src/assets/imgs/car-4.jpg'
+import car5 from '../../../../src/assets/imgs/car-5.jpg'
+
+
+
+import Carousel from "@/components/ImageSlider";
 
 
 const Home = () => {
+
+    const slides = [
+        car1,
+        car2,
+        car3,
+        car4,
+        car5,
+    ]
+
     return (
-<Card>    <Posts/>
-</Card>
+        <div className="relative">
+            <div className="max-w-lg">
+                <Carousel slides={slides}/>
+            </div>
+        </div>
 
     );
 };
